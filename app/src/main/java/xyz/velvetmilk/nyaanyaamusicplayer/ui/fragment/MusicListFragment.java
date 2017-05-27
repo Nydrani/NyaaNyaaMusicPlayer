8cba7ea1c9416d3a6736cdd849e9165f42a978e4
nydrani <victorzhang6897@gmail.com>
2017-05-28 00:21:08 +1000

    Attaching adapter to music list view

--- app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/ui/fragment/MusicListFragment.java
+++ app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/ui/fragment/MusicListFragment.java
@@ -1,10 +1,15 @@
 package xyz.velvetmilk.nyaanyaamusicplayer.ui.fragment;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import xyz.velvetmilk.nyaanyaamusicplayer.R;
 
@@ -19,6 +24,8 @@ public class MusicListFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private String[] elements = {"Element1", "Elements2", "Element3", "Element4"};
+
 
     public static MusicListFragment newInstance(String param1, String param2) {
         MusicListFragment fragment = new MusicListFragment();
@@ -37,7 +44,16 @@ public class MusicListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Activity activity = getActivity();
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_musiclist, container, false);
+        View view = inflater.inflate(R.layout.fragment_musiclist, container, false);
+
+        // get listview
+        ListView music_listview = (ListView)view.findViewById(R.id.music_list);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, android.R.id.text1, elements);
+        music_listview.setAdapter(adapter);
+
+        return view;
     }
 }
