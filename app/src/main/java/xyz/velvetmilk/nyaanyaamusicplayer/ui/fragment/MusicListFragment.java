9225d8214394419d8f6e5e42d6e865632e864f8c
nydrani <victorzhang6897@gmail.com>
2017-05-28 17:49:14 +1000

    Music fragment now uses the custom music loader to populate list

--- app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/ui/fragment/MusicListFragment.java
+++ app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/ui/fragment/MusicListFragment.java
@@ -3,29 +3,33 @@ package xyz.velvetmilk.nyaanyaamusicplayer.ui.fragment;
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.support.v4.content.ContextCompat;
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
 
 import xyz.velvetmilk.nyaanyaamusicplayer.R;
 import xyz.velvetmilk.nyaanyaamusicplayer.loader.MusicListLoader;
+import xyz.velvetmilk.nyaanyaamusicplayer.model.MusicPiece;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * to handle interaction events.
- * Use the {@link MusicListFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Created by nydrani on 28/05/17.
  */
-public class MusicListFragment extends Fragment {
+public class MusicListFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<MusicPiece>> {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    private String[] elements = {"Element1", "Elements2", "Element3", "Element4"};
+
+    private View rootView;
+    private ListView listView;
+    private ArrayAdapter<MusicPiece> adapter;
+
 
 
     public static MusicListFragment newInstance(String param1, String param2) {
@@ -38,23 +42,50 @@ public class MusicListFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Activity activity = getActivity();
+        adapter = new ArrayAdapter<MusicPiece>(activity, 0);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
+                             final Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.list_base, container, false);
+        TextView textView = (TextView)rootView.findViewById(R.id.list_base_name);
+        textView.setText(savedInstanceState.getString(ARG_PARAM1));
+        listView = (ListView)rootView.findViewById(R.id.list_base);
+        listView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<List<MusicPiece>> onCreateLoader (final int id, final Bundle args) {
         Activity activity = getActivity();
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_musiclist, container, false);
+        MusicListLoader loader = new MusicListLoader(activity);
+        return loader;
+    }
 
-        // get listview
-        ListView music_listview = (ListView)view.findViewById(R.id.music_list);
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, android.R.id.text1, elements);
-        music_listview.setAdapter(adapter);
+    @Override
+    public void onLoadFinished(final Loader<List<MusicPiece>> loader, final List<MusicPiece> data) {
+        if (data.isEmpty()) {
+            adapter.clear();
+            return;
+        }
 
-        return view;
+        adapter.addAll(data);
+    }
+
+    @Override
+    public void onLoaderReset(final Loader<List<MusicPiece>> loader) {
+        adapter.clear();
     }
 }
