565a9c327b52e9383c0e7a9438c0ccb85a2f9b5b
nydrani <victorzhang6897@gmail.com>
2017-05-28 22:23:04 +1000

    Added permission request for external storage

--- app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/activity/BaseActivity.java
+++ app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/activity/BaseActivity.java
@@ -1,9 +1,12 @@
 package xyz.velvetmilk.nyaanyaamusicplayer.activity;
 
+import android.Manifest;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +18,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import xyz.velvetmilk.nyaanyaamusicplayer.BuildConfig;
 import xyz.velvetmilk.nyaanyaamusicplayer.R;
 import xyz.velvetmilk.nyaanyaamusicplayer.ui.dialogfragment.AboutDialogFragment;
@@ -22,19 +28,55 @@ import xyz.velvetmilk.nyaanyaamusicplayer.ui.fragment.MusicListFragment;
 
 public class BaseActivity extends AppCompatActivity {
     private static final String TAG = BaseActivity.class.getSimpleName();
+    private static final int PERMISSION_REQUEST_CODE = 0;
+
+
+    //=========================================================================
+    // Activity lifecycle
+    //=========================================================================
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onCreate");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
 
-        setFragment(MusicListFragment.newInstance("ad", "bc"));
+        if (hasPermissions()) {
+            setFragment(MusicListFragment.newInstance());
+        }
     }
 
+
+    //=========================================================================
+    // Activity request permissions callbacks
+    //=========================================================================
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onRequestPermissionsResult(final int resultCode, final String[] permissions,
+                                           final int[] grantResults) {
+        if (BuildConfig.DEBUG) Log.w(TAG, "onCreateOptionsMenu");
+
+        switch (resultCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (checkPermissionGrantResults(grantResults)) {
+                    setFragment(MusicListFragment.newInstance());
+                } else {
+                    finish();
+                }
+                break;
+            default:
+                if (BuildConfig.DEBUG) Log.w(TAG, "onCreateOptionsMenu: Unhandled result code");
+        }
+    }
+
+
+    //=========================================================================
+    // Activity menu callbacks
+    //=========================================================================
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onCreateOptionsMenu");
 
         MenuInflater mi = getMenuInflater();
@@ -43,36 +85,38 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(final MenuItem item) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onOptionsItemSelected");
 
         int id = item.getItemId();
-        boolean ret = false;
 
         switch (id) {
             case R.id.actionbar_homelink:
                 Snackbar.make(findViewById(android.R.id.content), "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                ret = true;
-                break;
+                return true;
+
             case R.id.actionbar_settings:
-                Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG);
-                ret = true;
-                break;
+                Toast toast = Toast.makeText(this, R.string.app_name, Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+
             case R.id.actionbar_about:
                 DialogFragment about = AboutDialogFragment.newInstance();
                 setDialogFragment(about);
-                ret = true;
-                break;
-            default:
-                ret = super.onOptionsItemSelected(item);
-        }
+                return true;
 
-        return ret;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
-    protected void setDialogFragment(DialogFragment dialog) {
+    //=========================================================================
+    // Helper functions
+    //=========================================================================
+
+    protected void setDialogFragment(final DialogFragment dialog) {
         if (BuildConfig.DEBUG) Log.d(TAG, "setDialogFragment");
 
         FragmentManager fm = getFragmentManager();
@@ -82,7 +126,7 @@ public class BaseActivity extends AppCompatActivity {
         dialog.show(ft, null);
     }
 
-    protected void setFragment(Fragment fragment) {
+    protected void setFragment(final Fragment fragment) {
         if (BuildConfig.DEBUG) Log.d(TAG, "setFragment");
 
         FragmentManager fm = getFragmentManager();
@@ -90,4 +134,42 @@ public class BaseActivity extends AppCompatActivity {
         ft.replace(android.R.id.content, fragment);
         ft.commit();
     }
+
+    private boolean hasPermissions() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "hasPermissions");
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true;
+
+        String[] permissions = new String[1];
+        permissions[0] = Manifest.permission.READ_EXTERNAL_STORAGE;
+        List<String> permissionList = new ArrayList<String>();
+
+
+        for (String permission : permissions) {
+            if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionList.add(permission);
+            }
+        }
+
+        String[] neededPermissions = new String[permissionList.size()];
+        neededPermissions = permissionList.toArray(neededPermissions);
+
+        if (neededPermissions.length > 0) {
+            requestPermissions(neededPermissions, PERMISSION_REQUEST_CODE);
+        }
+
+        return neededPermissions.length == 0;
+    }
+
+    private boolean checkPermissionGrantResults(final int[] grantResults) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "checkPermissionGrantResults");
+
+        for (int result : grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
