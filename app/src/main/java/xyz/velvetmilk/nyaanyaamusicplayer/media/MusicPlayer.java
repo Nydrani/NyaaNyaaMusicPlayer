319d53f4bdaf499362675bfe78b7add1da79c6cc
nydrani <victorzhang6897@gmail.com>
2017-06-14 00:24:13 +1000

    Added audiofocus listener

--- app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/media/MusicPlayer.java
+++ app/src/main/java/xyz/velvetmilk/nyaanyaamusicplayer/media/MusicPlayer.java
@@ -1,8 +1,9 @@
 package xyz.velvetmilk.nyaanyaamusicplayer.media;
 
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer;
+import android.media.session.MediaSession;
 import android.util.Log;
 
 import java.io.IOException;
@@ -17,16 +18,19 @@ import xyz.velvetmilk.nyaanyaamusicplayer.service.MusicPlaybackService;
 public class MusicPlayer implements
         MediaPlayer.OnCompletionListener,
         MediaPlayer.OnErrorListener,
-        MediaPlayer.OnPreparedListener {
+        MediaPlayer.OnPreparedListener,
+        AudioManager.OnAudioFocusChangeListener {
     private static final String TAG = MusicPlayer.class.getSimpleName();
-    private MusicPlaybackService musicPlaybackService;
     private MediaPlayer mediaPlayer;
     private AudioAttributes audioAttributes;
+    private AudioManager audioManager;
+    private MediaSession mediaSession;
 
-    public MusicPlayer(MusicPlaybackService service) {
+    public MusicPlayer(AudioManager audioManager, MediaSession mediaSession) {
         if (BuildConfig.DEBUG) Log.d(TAG, "constructor");
 
-        musicPlaybackService = service;
+        this.audioManager = audioManager;
+        this.mediaSession = mediaSession;
         audioAttributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
                 .build();
@@ -40,6 +44,8 @@ public class MusicPlayer implements
     // ========================================================================
 
     private void initMediaPlayer() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "initMediaPlayer");
+
         mediaPlayer = new MediaPlayer();
 
         mediaPlayer.setOnCompletionListener(this);
@@ -67,6 +73,16 @@ public class MusicPlayer implements
             mediaPlayer.start();
         } catch (IllegalStateException e) {
             if (BuildConfig.DEBUG) Log.d(TAG, "Called start in illegal state");
+        }
+    }
+
+    public void pause() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "pause");
+
+        try {
+            mediaPlayer.pause();
+        } catch (IllegalStateException e) {
+            if (BuildConfig.DEBUG) Log.d(TAG, "Called pause in illegal state");
         }
     }
 
@@ -101,10 +117,13 @@ public class MusicPlayer implements
     public boolean onError(MediaPlayer mp, int what, int extra) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onError");
 
+        audioManager.abandonAudioFocus(this);
+        mediaSession.setActive(false);
+
         switch (what) {
             case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
                 if (BuildConfig.DEBUG) Log.e(TAG, "MEDIA_ERROR_SERVER_DIED");
-                mediaPlayer.release();
+                release();
                 initMediaPlayer();
                 return true;
             default:
@@ -117,11 +136,66 @@ public class MusicPlayer implements
     @Override
     public void onPrepared(MediaPlayer mp) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onPrepared");
+
+        int status = audioManager.requestAudioFocus(this,
+                AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+        if (status == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            mp.stop();
+            return;
+        }
+
+        mediaSession.setActive(true);
         mp.start();
     }
 
     @Override
     public void onCompletion(MediaPlayer mp) {
         if (BuildConfig.DEBUG) Log.d(TAG, "onCompletion");
+
+        audioManager.abandonAudioFocus(this);
+        mediaSession.setActive(false);
+    }
+
+
+    // ========================================================================
+    // AudioManager listener overrides
+    // ========================================================================
+
+    @Override
+    public void onAudioFocusChange(int change) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onAudioFocusChange");
+
+        switch (change) {
+            case AudioManager.AUDIOFOCUS_GAIN:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_GAIN");
+                mediaPlayer.setVolume(1.0f, 1.0f);
+                start();
+                break;
+            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_GAIN_TRANSIENT");
+                break;
+            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE");
+                break;
+            case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK");
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_LOSS");
+                stop();
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_LOSS_TRANSIENT");
+                pause();
+                break;
+            case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                if (BuildConfig.DEBUG) Log.d(TAG, "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
+                mediaPlayer.setVolume(0.2f, 0.2f);
+                break;
+            default:
+                if (BuildConfig.DEBUG) Log.wtf(TAG, "VERY BAD HAPPENED");
+                break;
+        }
     }
 }
