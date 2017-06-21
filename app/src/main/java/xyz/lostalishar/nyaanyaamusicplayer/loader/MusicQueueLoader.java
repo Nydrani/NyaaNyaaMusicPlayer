d9f6fa86c90d0efa65d2247ad0c0b5e91b352191
nydrani <victorzhang6897@gmail.com>
2017-06-21 16:53:11 +1000

    Added initial MusicQueue files

---
+++ app/src/main/java/xyz/lostalishar/nyaanyaamusicplayer/loader/MusicQueueLoader.java
@@ -0,0 +1,105 @@
+package xyz.lostalishar.nyaanyaamusicplayer.loader;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.lostalishar.nyaanyaamusicplayer.BuildConfig;
+import xyz.lostalishar.nyaanyaamusicplayer.model.Music;
+
+/**
+ * Loads a List of Music classes from the MediaStore cursor in the background
+ */
+
+public class MusicQueueLoader extends CachedAsyncTaskLoader<List<Music>> {
+    private static final String TAG = MusicQueueLoader.class.getSimpleName();
+
+    private List<Music> musicList;
+
+    public MusicQueueLoader(Context context) {
+        super(context);
+        if (BuildConfig.DEBUG) Log.d(TAG, "constructor");
+
+        musicList = new ArrayList<>();
+    }
+
+
+    //=========================================================================
+    // AsyncTaskLoader implementation
+    //=========================================================================
+
+    @Override
+    public List<Music> loadInBackground() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "loadInBackground");
+
+        Cursor cursor = getCursor();
+
+        // can sometimes return null on bad problems
+        if (cursor == null) {
+            return musicList;
+        }
+
+        int idColumn = cursor.getColumnIndex(MediaStore.Audio.Media._ID);
+        int titleColumn = cursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
+        int artistColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+        int albumColumn = cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM);
+        int durationColumn = cursor.getColumnIndex(MediaStore.Audio.Media.DURATION);
+        int mimeTypeColumn = cursor.getColumnIndex(MediaStore.Audio.Media.MIME_TYPE);
+
+        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+            final long id = cursor.getLong(idColumn);
+            final String name = cursor.getString(titleColumn);
+            final String artistName = cursor.getString(artistColumn);
+            final String albumName = cursor.getString(albumColumn);
+            final long duration = cursor.getLong(durationColumn);
+            final String mimeType = cursor.getString(mimeTypeColumn);
+
+            final int durationInSecs = (int) duration / 1000;
+            final Music music = new Music(id, name, artistName, albumName, durationInSecs,
+                    mimeType);
+
+            musicList.add(music);
+        }
+
+        cursor.close();
+
+        return musicList;
+    }
+
+
+    //=========================================================================
+    // Helper functions
+    //=========================================================================
+
+    private Cursor getCursor() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "getCursor");
+
+        Context context = getContext();
+        return makeMusicCursor(context);
+    }
+
+    private static Cursor makeMusicCursor(Context context) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "makeMusicCursor");
+
+        ContentResolver musicResolver = context.getContentResolver();
+        Uri musicUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = new String[6];
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "=1";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " DESC";
+
+        projection[0] = MediaStore.Audio.Media._ID;
+        projection[1] = MediaStore.Audio.Media.TITLE;
+        projection[2] = MediaStore.Audio.Media.ARTIST;
+        projection[3] = MediaStore.Audio.Media.ALBUM;
+        projection[4] = MediaStore.Audio.Media.DURATION;
+        projection[5] = MediaStore.Audio.Media.MIME_TYPE;
+
+        return musicResolver.query(musicUri, projection, selection, null, sortOrder);
+    }
+}
---
+++ app/src/main/java/xyz/lostalishar/nyaanyaamusicplayer/ui/fragment/MusicQueueFragment.java
@@ -0,0 +1,104 @@
+package xyz.lostalishar.nyaanyaamusicplayer.ui.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.lostalishar.nyaanyaamusicplayer.BuildConfig;
+import xyz.lostalishar.nyaanyaamusicplayer.R;
+import xyz.lostalishar.nyaanyaamusicplayer.adapter.MusicAdapter;
+import xyz.lostalishar.nyaanyaamusicplayer.loader.MusicQueueLoader;
+import xyz.lostalishar.nyaanyaamusicplayer.model.Music;
+
+/**
+ * Fragment containing the current play queue
+ */
+
+public class MusicQueueFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Music>> {
+    private static final String TAG = MusicQueueFragment.class.getSimpleName();
+
+    private RecyclerView.LayoutManager layout;
+    private MusicAdapter adapter;
+
+    public static MusicQueueFragment newInstance() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "newInstance");
+
+        return new MusicQueueFragment();
+    }
+
+
+    //=========================================================================
+    // Fragment lifecycle
+    //=========================================================================
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+
+        Activity activity = getActivity();
+        List<Music> musicList = new ArrayList<>();
+        adapter = new MusicAdapter(musicList);
+        layout = new LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onCreateView");
+
+        View rootView = inflater.inflate(R.layout.list_base, container, false);
+        RecyclerView recyclerView = (RecyclerView)rootView.findViewById(R.id.list_base);
+        recyclerView.setLayoutManager(layout);
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onActivityCreated");
+        super.onActivityCreated(savedInstanceState);
+
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+
+    //=========================================================================
+    // LoaderManager.LoaderCallbacks
+    //=========================================================================
+
+    @Override
+    public Loader<List<Music>> onCreateLoader(int id, Bundle args) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onCreateLoader");
+
+        Activity activity = getActivity();
+
+        return new MusicQueueLoader(activity);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Music>> loader, List<Music> data) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onLoadFinished");
+
+        adapter.swap(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Music>> loader) {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onLoadReset");
+
+        adapter.swap(null);
+    }
+}
