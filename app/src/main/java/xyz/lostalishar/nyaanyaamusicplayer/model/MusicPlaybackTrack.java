9e7ba75edb20498b3fc068dd1ffe5b6515ec50ce
nydrani <victorzhang6897@gmail.com>
2017-07-28 19:22:05 +1000

    Overridden equals for comparison between music tracks

--- app/src/main/java/xyz/lostalishar/nyaanyaamusicplayer/model/MusicPlaybackTrack.java
+++ app/src/main/java/xyz/lostalishar/nyaanyaamusicplayer/model/MusicPlaybackTrack.java
@@ -58,4 +58,25 @@ public class MusicPlaybackTrack implements Parcelable {
     public String toString() {
         return "TrackId: " + String.valueOf(id);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final MusicPlaybackTrack other = (MusicPlaybackTrack) obj;
+
+        if (id != other.id) {
+            return false;
+        }
+
+        return true;
+    }
 }
