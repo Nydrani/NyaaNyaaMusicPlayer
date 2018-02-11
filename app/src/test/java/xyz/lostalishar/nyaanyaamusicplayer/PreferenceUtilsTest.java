527ec233dd2191b73ae21d3ab7a6921f45448749
nydrani <victorzhang6897@gmail.com>
2018-02-11 16:43:09 +1100

    Updated yaml

--- app/src/test/java/xyz/lostalishar/nyaanyaamusicplayer/PreferenceUtilsTest.java
+++ app/src/test/java/xyz/lostalishar/nyaanyaamusicplayer/PreferenceUtilsTest.java
@@ -16,11 +16,6 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class PreferenceUtilsTest {
     @Mock
     private SharedPreferences sharedPreferences;
