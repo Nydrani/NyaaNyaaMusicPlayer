bd0a6a114466f7d2ddf00afc4a1e6b39656ce73a
nydrani <victorzhang6897@gmail.com>
2018-03-18 18:44:56 +1100

    Added Preferences first run initialisation

---
+++ app/src/main/java/xyz/lostalishar/nyaanyaamusicplayer/App.java
@@ -0,0 +1,23 @@
+package xyz.lostalishar.nyaanyaamusicplayer;
+
+import android.app.Application;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class App extends Application {
+    public static final String TAG = App.class.getSimpleName();
+
+
+    //=========================================================================
+    // Application lifecycle
+    //=========================================================================
+
+    @Override
+    public void onCreate() {
+        if (BuildConfig.DEBUG) Log.d(TAG, "onCreate");
+        super.onCreate();
+
+        // set default values only once on application start
+        PreferenceManager.setDefaultValues(getApplicationContext(), R.xml.settings_layout, false);
+    }
+}
